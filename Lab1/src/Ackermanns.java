035baaf4f0292cbe41e24451b0b721c77307b678
zachary0191 <zachary0191@zachs-mbp.gha.chartermi.net>
2014-01-23 10:27:36 -0500

    Create Lab1

---
+++ Lab1/src/Ackermanns.java
@@ -0,0 +1,17 @@
+/* LAB # 
+*FALL 2013 
+*STUDENTÕS FIRST NAME, LAST NAME 
+* Following program is my own effort/work. 
+*I did not copy this program or program segment from anyone or any website site. If proven 
+otherwise I will accept the consequences and the actions that will be taken for cheating in this 
+class. 
+*/ 
+public class Ackermanns 
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
